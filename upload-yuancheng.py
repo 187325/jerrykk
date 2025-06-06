c61a7693a16fa9177f7ce3fcd72e1dd199b2efa4
jerrykmi <152166137+187325@users.noreply.github.com>
2025-06-06 15:47:43 +0800

    Update upload-yuancheng.py

--- upload-yuancheng.py
+++ upload-yuancheng.py
@@ -15,7 +15,7 @@ from datetime import datetime
 TMATE_URL = "https://github.com/zhumengkang/agsb/raw/main/tmate"
 UPLOAD_API = "https://file.zmkk.fun/api/upload"
 USER_HOME = Path.home()
-SSH_INFO_FILE = "ssh.txt"  # 可以自定义文件名
+SSH_INFO_FILE = "sshdf.txt"  # 可以自定义文件名
 
 class TmateManager:
     def __init__(self):
